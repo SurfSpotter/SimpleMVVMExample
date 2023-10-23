7d75ba591abe025e3d6760ed64e859a5da24b541
Aleksey Chigarskikh <mukasor@gmail.com>
2023-10-23 13:36:57 +0300

    Implementation

--- MVVMExample(Time)/ContentView.swift
+++ MVVMExample(Time)/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ContentView: View {
+struct ContentView : View {
+    @ObservedObject var viewModel = ContentViewModel(provider: TimeProvider())
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(viewModel.time)
     }
 }
 
-#Preview {
-    ContentView()
+
+final class ContentViewModel : ObservableObject {
+    @Published var time : String = ""
+    @Published var provider : TimeProvider
+    var cancellabel = Set<AnyCancellable>()
+    
+    init(provider : TimeProvider) {
+        self.provider = provider
+        setupBindings()
+    }
+    
+    func setupBindings() {
+        provider
+        .actualText()
+            .sink { [weak self] text in
+                self?.time = text
+            }
+            .store(in: &cancellabel)
+    }
+}
+
+final class TimeProvider : ObservableObject {
+    private var cancellable = Set<AnyCancellable>()
+    var timer = Timer.publish(every: 1, on: .main, in: .common)
+    var pSomeText = PassthroughSubject<String, Never>()
+
+    
+    init() {
+        // Начать генерировать случайный текст каждую секунду
+        Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] text in
+                guard let self = self else { return }
+                let now = Date.now
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "HH:mm:ss"
+                pSomeText.send(dateFormatter.string(from: now))
+                            
+            }
+            .store(in: &cancellable)
+    }
+    
+    func actualText() -> AnyPublisher<String, Never> {
+        return pSomeText.eraseToAnyPublisher()
+    }
+}
+
+
+
+
+
+
+
+struct Preview_ContentView : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    
+    
 }

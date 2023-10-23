62becaecdb2a5516458120318868f3e0508c5391
Aleksey Chigarskikh <mukasor@gmail.com>
2023-10-23 13:27:27 +0300

    Initial Commit

---
+++ MVVMExample(Time)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMExample(Time)
+//
+//  Created by Алексей Чигарских on 23.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMExample(Time)/MVVMExample_Time_App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExample_Time_App.swift
+//  MVVMExample(Time)
+//
+//  Created by Алексей Чигарских on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExample_Time_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
